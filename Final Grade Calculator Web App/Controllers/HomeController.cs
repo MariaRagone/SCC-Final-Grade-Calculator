11dd628ec5bbe6a4c6b15b7d34b0f5ad50e92242
Maria Ragone <maria@ragone.net>
2023-08-10 00:06:19 -0400

    version 1.0

---
+++ Final Grade Calculator Web App/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using Final_Grade_Calculator_Web_App.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Final_Grade_Calculator_Web_App.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Grades(Student s)
+        {
+            return View(s);
+        }
+
+     
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Final Grade Calculator Web App/Models/Student.cs
@@ -0,0 +1,11 @@
+﻿namespace Final_Grade_Calculator_Web_App.Models
+{
+    public class Student
+    {
+        public string studentName { get; set; }
+        public double studentPercentage { get; set; }
+        public string studentLetterGrade { get; set; }
+        public decimal studentGPA { get; set;}
+    }
+
+}
